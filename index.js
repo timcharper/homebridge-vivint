c31866ffd1cdf0451f4927ce5545bba978ad150a
Alexandr Balan <balansse@gmail.com>
2020-09-08 23:01:54 -0500

    Added support for CO/Smoke/Heat/Freeze detectors; Fixed "lock jammed" notification parsing; Read panel state after set from PubNub response; VivintApi refactor using async/await instead of promises;

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ module.exports = function (homebridge) {
       let config_apiLoginRefreshSecs = config.apiLoginRefreshSecs || 1200 // once per 20 minutes default
 
       let VivintApi = VivintApiModule(config, log)
-      this.vivintApiPromise = VivintApi.login({username: config.username, password: config.password}, 1)
+      this.vivintApiPromise = VivintApi.login({username: config.username, password: config.password})
 
       this.pubNubPromise = this.vivintApiPromise
         .then((vivintApi) => vivintApi.connectPubNub())
